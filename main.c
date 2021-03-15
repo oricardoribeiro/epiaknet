51df152a4ea802b180001150ee06553c482c0892
Onildo R Ribeiro <oricardoribeiro.sa@gmail.com>
2021-03-15 16:39:40 -0300

    main.c modified.

--- main.c
+++ main.c
@@ -9,12 +9,12 @@
 
 int main(int argc, char *argv[])
 {
-	system("reset");
-
 	if (argc != 2)
 	{
+		printf("Usage: epiaknet <IP Address>\n\n");
+		printf("Epiaknet is a simple network tool in order to discovery ports opened.\n\n");
+		printf("Example: epiaknet 192.168.0.1\n\n");
 		printf("Epiaknet V 0.0.1 - A network mapper\n");
-		printf("Error: Usage - epiaknet <IP Address>\n\n");
 	}
 
 	return(0);
