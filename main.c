10d9918abc2587ccfafb5753e0fde2010b5d0074
Onildo R Ribeiro <oricardoribeiro.sa@gmail.com>
2021-03-15 14:30:10 -0300

    uploading main.c

--- main.c
+++ main.c
@@ -9,5 +9,13 @@
 
 int main(int argc, char *argv[])
 {
+	system("reset");
+
+	if (argc != 2)
+	{
+		printf("Epiaknet V 0.0.1 - A network mapper\n");
+		printf("Error: Usage - epiaknet <IP Address>\n\n");
+	}
+
 	return(0);
 }
